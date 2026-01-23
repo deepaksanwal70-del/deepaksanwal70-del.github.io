24c35c0a261fcd8be294b06605a1729665604c87
Deepak Sanwal <deepaksanwal70@gmail.com>
2026-01-23 17:15:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ function reveal() {
 
 reveal(); // page load par bhi run
 
+const sections = document.querySelectorAll("section, header");
+const navLinks = document.querySelectorAll(".navbar ul li a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 120;
+    const sectionHeight = section.offsetHeight;
+
+    if (scrollY >= sectionTop && scrollY < sectionTop + sectionHeight) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
+
+
+
